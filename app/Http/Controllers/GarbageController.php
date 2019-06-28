5f2dcabd9fa39460275b53b754d5b6bbc24fd3b0
manzhouya <manzhouya@gmail.com>
2019-06-28 10:13:32 +0800

    adjust

--- app/Http/Controllers/GarbageController.php
+++ app/Http/Controllers/GarbageController.php
@@ -6,8 +6,12 @@ use Illuminate\Http\Request;
 
 class GarbageController extends Controller
 {
-    public function show($name)
+    public function show($name=null)
     {
+      	$name = trim($name);
+      	if(!$name){
+        	return [];
+        }
         $garbages = \DB::select("SELECT `garbages`.`name` AS `name`, `categories`.`name` AS `category` FROM `garbages` JOIN `categories` ON `garbages`.`category_id` = `categories`.`id` WHERE `garbages`.`name` LIKE '%$name%'");
     	return $garbages;
     }
--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('garbages/{name}', 'GarbageController@show');
+Route::get('garbages/{name?}', 'GarbageController@show');
