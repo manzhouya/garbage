9ae31c19bca44478887754c52b2b80efba49321a
manzhouya <manzhouya@gmail.com>
2019-07-03 17:03:05 +0800

    perfect

--- app/Http/Controllers/GarbageController.php
+++ app/Http/Controllers/GarbageController.php
@@ -15,11 +15,12 @@ class GarbageController extends Controller
         $garbages = \DB::select("SELECT `garbages`.`name` AS `name`, `categories`.`name` AS `category`, `categories`.`style` AS `category_style` FROM `garbages` JOIN `categories` ON `garbages`.`category_id` = `categories`.`id` WHERE `garbages`.`name` LIKE '%$name%'");
     	
       	if($garbages){
+          $time = date("Y-m-d H:i:s");
           $k = \DB::select("SELECT `id` FROM `keywords` WHERE `name` = '$name'");
           if($k){
               \DB::update("UPDATE `keywords` SET `count` = count + 1 WHERE `name` = '$name'");
           }else{
-              \DB::insert("INSERT INTO `keywords` (`name`, `count`) values ('$name', 1)");
+              \DB::insert("INSERT INTO `keywords` (`name`, `count`, `created_at`, `updated_at`) values ('$name', 1, '$time', '$time')");
           }
           //if(\DB::select("SELECT `id` FROM `keywords` WHERE `name` LIKE '%$name'")){
           //  \DB::delete("DELETE FROM `keywords` WHERE `name` = '$name'");
