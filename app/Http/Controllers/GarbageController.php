d07db6aa0a69cba4c95a2bbd36dc8ec9bd6fa4e1
manzhouya <manzhouya@gmail.com>
2019-07-06 18:25:04 +0800

    perfect

--- app/Http/Controllers/GarbageController.php
+++ app/Http/Controllers/GarbageController.php
@@ -3,45 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Cache;
 
 class GarbageController extends Controller
 {
-    public function show($name=null)
+    public function show($name = null)
     {
-      	$name = trim($name);
-      	if(!$name){
-        	return [];
+        $name = trim($name);
+        if (!$name) {
+            return [];
         }
         $garbages = \DB::select("SELECT `garbages`.`name` AS `name`, `categories`.`name` AS `category`, `categories`.`style` AS `category_style` FROM `garbages` JOIN `categories` ON `garbages`.`category_id` = `categories`.`id` WHERE `garbages`.`name` LIKE '%$name%'");
-    	
-      	if($garbages){
-          $time = date("Y-m-d H:i:s");
-          $k = \DB::select("SELECT `id` FROM `keywords` WHERE `name` = '$name'");
-          if($k){
-              \DB::update("UPDATE `keywords` SET `count` = count + 1 WHERE `name` = '$name'");
-          }else{
-              \DB::insert("INSERT INTO `keywords` (`name`, `count`, `created_at`, `updated_at`) values ('$name', 1, '$time', '$time')");
-          }
-          //if(\DB::select("SELECT `id` FROM `keywords` WHERE `name` LIKE '%$name'")){
-          //  \DB::delete("DELETE FROM `keywords` WHERE `name` = '$name'");
-          //}
+
+        if ($garbages) {
+            $time = date("Y-m-d H:i:s");
+            $k = \DB::select("SELECT `id` FROM `keywords` WHERE `name` = '$name'");
+            if ($k) {
+                \DB::update("UPDATE `keywords` SET `count` = count + 1 WHERE `name` = '$name'");
+            } else {
+                \DB::insert("INSERT INTO `keywords` (`name`, `count`, `created_at`, `updated_at`) values ('$name', 1, '$time', '$time')");
+            }
         }
-      	return $garbages;
+
+        return $garbages;
     }
-  
-  	public function index()
+
+    public function index()
     {
-        $garbages = json_decode(Redis::get('garbages'));
-      	if(!$garbages){
-        	$garbages = \DB::select("SELECT * FROM `garbages`");
-            Redis::set('garbages', json_encode($garbages));
-        }
-      
-        $garbages = \Cache::remember('garbages', 24 * 60 * 60 * 7, function () {
-              return \DB::select("SELECT * FROM `garbages`");
+//        $garbages = json_decode(Redis::get('garbages'));
+//        if (!$garbages) {
+//            $garbages = \DB::select("SELECT * FROM `garbages`");
+//            Redis::set('garbages', json_encode($garbages));
+//        }
+
+        $garbages = Cache::remember('garbages', 24 * 60 * 60 * 7, function () {
+            return \DB::select("SELECT * FROM `garbages`");
         });
-      
-      	return $garbages;
+
+        return $garbages;
     }
 }
--- app/Http/Controllers/SubmitsController.php
+++ app/Http/Controllers/SubmitsController.php
@@ -8,16 +8,16 @@ class SubmitsController extends Controller
 {
     public function create(Request $request)
     {
-      	$time = date("Y-m-d H:i:s");
-      	$res = \DB::insert("INSERT INTO `submits` (`name`, `created_at`, `updated_at`) values ('$request->name', '$time', '$time')");
-      
-      	if($res){
-    		return response()->json([
-            	'code' => 1
+        $time = date("Y-m-d H:i:s");
+        $res = \DB::insert("INSERT INTO `submits` (`name`, `created_at`, `updated_at`) values ('$request->name', '$time', '$time')");
+
+        if ($res) {
+            return response()->json([
+                'code' => 1
             ]);
-        }else{
-    		return response()->json([
-            	'code' => 0
+        } else {
+            return response()->json([
+                'code' => 0
             ]);
         }
     }
